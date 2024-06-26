f5fc4958a567f9ff379932b0214e285bce2f20d3
Asghar Ali <fa21bscs0017@maju.edu.pk>
2024-06-26 17:32:53 +0500

    vercel deployee

--- server.js
+++ server.js
@@ -1,26 +1,4 @@
-const fastify = require('fastify')({ logger: true });
-const dataSource = require('./src/infrastructure/psql');
 
-// const contactRoute = require('./src/routes/contactRoutes');
+const StartServer=require('./src/index')
 
-
-
-fastify.get('/', async (request, reply) => {
-  reply.send({ hello: 'world' });
-});
-
-// fastify.register(contactRoute);
-
-
-const start = async () => {
-  try {
-    await dataSource.initialize();
-    // console.log("Database connection has been established");
-    await fastify.listen(4000, '0.0.0.0');
-    fastify.log.info(`Server is running at http://localhost:4000`);
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-};
-start();
+StartServer();
