5a2ad298b70ac17c4eba7dafbe7d4c864d61a92e
Asghar Ali <fa21bscs0017@maju.edu.pk>
2024-06-27 03:24:59 +0500

    vercel deployee

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config();
 
-const StartServer=require('./src/App')
+const fastify = require('./src/App');
+const dataSource = require('./src/infrastructure/psql');
+const { logger } = require('./logger');
 
-StartServer();
+const startServer = async () => {
+  try {
+    await dataSource.initialize();
+    logger.info("Database connection has been established");
+
+    fastify.listen(process.env.PORT || 4000, '0.0.0.0', (err, address) => {
+      if (err) {
+        logger.error(err.message);
+        process.exit(1);
+      }
+      logger.info(`Server is listening on ${address}`);
+    });
+
+  } catch (error) {
+    logger.error(error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
