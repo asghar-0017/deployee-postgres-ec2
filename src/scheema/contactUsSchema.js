d1ec6a0c3be10b89ea89201e7f0058df838001b7
asghar-0017 <asghar.usman0017@gmail.com>
2024-09-02 18:11:11 +0500

    webite

--- src/scheema/contactUsSchema.js
+++ src/scheema/contactUsSchema.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 
+
 const ValidateContact = Joi.object({
   name: Joi.string().required(), // Allow empty string for name
   email: Joi.string()
@@ -10,17 +11,11 @@ const ValidateContact = Joi.object({
       'any.required': 'Email is required'
     }),
   company: Joi.string().allow('').optional(),
-  website: Joi.string()
-    .pattern(/^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}\/?$/) // Regex for validating a website URL
-    .allow('')
-    .optional()
-    .messages({
-      'string.pattern.base': 'Please provide a valid website URL'
-    }),
+  website: Joi.string().allow('').optional(),
   phone: Joi.number().required(),
   message: Joi.string().required(),
   serviceType: Joi.string().required()
-})
+});
 
 module.exports = {
   ValidateContact
